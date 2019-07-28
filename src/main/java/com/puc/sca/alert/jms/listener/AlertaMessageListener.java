cca29a78136fd5c8b52bb5b9ff0e729ea931fba9
breno500as <breno500as@gmail.com>
2019-07-28 20:22:43 -0300

    monitoramento

--- src/main/java/com/puc/sca/alert/jms/listener/AlertaMessageListener.java
+++ src/main/java/com/puc/sca/alert/jms/listener/AlertaMessageListener.java
@@ -3,14 +3,15 @@ package com.puc.sca.alert.jms.listener;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
-import com.puc.sca.integration.pojo.Alerta;
+import com.puc.sca.integration.util.Alerta;
+import com.puc.sca.integration.util.Constants;
 
 
 
 @Component
 public class AlertaMessageListener {
-
-	@JmsListener(destination = "topic.alerta-box")
+	
+	@JmsListener(destination = Constants.TOPICO_MODULO_SEGURACA)
 	public void receiveAlert(Alerta alerta) {
        System.out.println(alerta.getNivel());
 	}
