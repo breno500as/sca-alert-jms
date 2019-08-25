a52987f4704a378ee0c63f6e37089f461d917e00
breno500as <breno500as@github.com>
2019-08-24 21:37:39 -0300

    Documentação

--- src/main/java/com/puc/sca/alert/jms/ScaAlertJmsApplication.java
+++ src/main/java/com/puc/sca/alert/jms/ScaAlertJmsApplication.java
@@ -4,6 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * URL do message broker para configurações http://localhost:8161/admin
+ * usuário: admin 
+ * senha: admin
+ * @author Breno
+ *
+ */
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ScaAlertJmsApplication {
