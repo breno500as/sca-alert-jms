21116859c23a59e0b9426cbafbe9bf8065cd0307
breno500as <breno500as@github.com>
2019-10-20 19:02:40 -0300

    documentação

--- src/main/java/com/puc/sca/alert/jms/ScaAlertJmsApplication.java
+++ src/main/java/com/puc/sca/alert/jms/ScaAlertJmsApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
+ * Baixar o activemq: https://activemq.apache.org/components/classic/download/
+ * Descompactar
+ * Ir na basta bin  executar o comando: sudo ./activemq start
  * URL do message broker para configurações http://localhost:8161/admin
  * usuário: admin 
  * senha: admin
